7ff359b2df86447c7e688d998da67dd40b1a9729
rakesh-ganganapalli-au13 <70137055+rakesh-ganganapalli-au13@users.noreply.github.com>
2024-02-07 18:30:13 +0000

    Delete assignment directory

--- assignment/week25-day1-assignment/src/database/mangoose.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose")
-
-
-mongoose.connect('mongodb://localhost:27017/rakesh',{useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=>{console.log("database connected sucsussfully")})
-.catch(()=>console.log("database not connected"))
